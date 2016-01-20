4b8fe7990ac8e719537b201dde5ca6551a544d74
sue445 <sue445@sue445.net>
2016-01-20 22:15:44 +0900

    Add test case for absolute and relative path

--- test/test_jenkins-backup.rb
+++ test/test_jenkins-backup.rb
@@ -11,6 +11,24 @@ class TestJenkinsBackup < Test::Unit::TestCase
     `tar tfz #{tar_file}`.split(/[\r\n]+/)
   end
 
+  def use_absolute_dist_file
+    @dist_file = "#{@temp_dir}/archive.tar.gz"
+  end
+
+  def use_relative_dist_file
+    @dist_file = "archive.tar.gz"
+  end
+
+  def use_jenkins_home_without_pinned
+    # jenkins_home1 dir does not contain dummy.hpi.pinned and dummy.jpi.pinned
+    @jenkins_home = File.join(TEST_DIR, "jenkins_home1")
+  end
+
+  def use_jenkins_home_with_pinned
+    # jenkins_home2 dir contain dummy.hpi.pinned and dummy.jpi.pinned
+    @jenkins_home = File.join(TEST_DIR, "jenkins_home2")
+  end
+
   around do |test|
     Dir.mktmpdir("test-") do |temp_dir|
       @temp_dir = temp_dir
@@ -24,81 +42,149 @@ class TestJenkinsBackup < Test::Unit::TestCase
     end
   end
 
-  sub_test_case "When not exists pinned files" do
-    setup do
-      # jenkins_home1 dir does not contain dummy.hpi.pinned and dummy.jpi.pinned
-      @jenkins_home = File.join(TEST_DIR, "jenkins_home1")
+  sub_test_case "When use absolute path" do
+    sub_test_case "When not exists pinned files" do
+      test "should create archive file" do
+        use_absolute_dist_file
+        use_jenkins_home_without_pinned
+
+        assert "jenkins-backup.sh should be success" do
+          system("#{SCRIPT_FILE} #{@jenkins_home} #{@dist_file}") == true
+        end
+
+        assert { File.exists?(@dist_file) }
+        assert { File.size(@dist_file) > 0 }
+
+        actual_files = list_tar_file(@dist_file)
+        expected_files = %w(
+          jenkins-backup/hudson.model.UpdateCenter.xml
+          jenkins-backup/jobs/
+          jenkins-backup/jobs/example_job/
+          jenkins-backup/jobs/example_job/config.xml
+          jenkins-backup/plugins/
+          jenkins-backup/plugins/dummy.hpi
+          jenkins-backup/plugins/dummy.jpi
+          jenkins-backup/secrets/
+          jenkins-backup/secrets/master.key
+          jenkins-backup/users/
+          jenkins-backup/users/sue445/
+          jenkins-backup/users/sue445/config.xml
+        )
+        expected_files.each do |file|
+          assert("archive file should include #{file}") do
+            actual_files.include?(file)
+          end
+        end
+      end
     end
 
-    test "should create archive file" do
-      dist_file = "#{@temp_dir}/archive.tar.gz"
+    sub_test_case "When exists pinned files" do
+      test "should create archive file" do
+        use_absolute_dist_file
+        use_jenkins_home_with_pinned
 
-      assert "jenkins-backup.sh should be success" do
-        system("#{SCRIPT_FILE} #{@jenkins_home} #{dist_file}") == true
-      end
+        assert "jenkins-backup.sh should be success" do
+          system("#{SCRIPT_FILE} #{@jenkins_home} #{@dist_file}") == true
+        end
 
-      assert { File.exists?(dist_file) }
-      assert { File.size(dist_file) > 0 }
+        assert { File.exists?(@dist_file) }
+        assert { File.size(@dist_file) > 0 }
 
-      actual_files = list_tar_file(dist_file)
-      expected_files = %w(
-        jenkins-backup/hudson.model.UpdateCenter.xml
-        jenkins-backup/jobs/
-        jenkins-backup/jobs/example_job/
-        jenkins-backup/jobs/example_job/config.xml
-        jenkins-backup/plugins/
-        jenkins-backup/plugins/dummy.hpi
-        jenkins-backup/plugins/dummy.jpi
-        jenkins-backup/secrets/
-        jenkins-backup/secrets/master.key
-        jenkins-backup/users/
-        jenkins-backup/users/sue445/
-        jenkins-backup/users/sue445/config.xml
-      )
-      expected_files.each do |file|
-        assert("archive file should include #{file}") do
-          actual_files.include?(file)
+        actual_files = list_tar_file(@dist_file)
+        expected_files = %w(
+          jenkins-backup/hudson.model.UpdateCenter.xml
+          jenkins-backup/jobs/
+          jenkins-backup/jobs/example_job/
+          jenkins-backup/jobs/example_job/config.xml
+          jenkins-backup/plugins/
+          jenkins-backup/plugins/dummy.hpi
+          jenkins-backup/plugins/dummy.hpi.pinned
+          jenkins-backup/plugins/dummy.jpi
+          jenkins-backup/plugins/dummy.jpi.pinned
+          jenkins-backup/secrets/
+          jenkins-backup/secrets/master.key
+          jenkins-backup/users/
+          jenkins-backup/users/sue445/
+          jenkins-backup/users/sue445/config.xml
+        )
+        expected_files.each do |file|
+          assert("archive file should include #{file}") do
+            actual_files.include?(file)
+          end
         end
       end
     end
   end
 
-  sub_test_case "When exists pinned files" do
-    setup do
-      # jenkins_home2 dir contain dummy.hpi.pinned and dummy.jpi.pinned
-      @jenkins_home = File.join(TEST_DIR, "jenkins_home2")
+  sub_test_case "When use relative path" do
+    sub_test_case "When not exists pinned files" do
+      test "should create archive file" do
+        use_relative_dist_file
+        use_jenkins_home_without_pinned
+
+        assert "jenkins-backup.sh should be success" do
+          system("#{SCRIPT_FILE} #{@jenkins_home} #{@dist_file}") == true
+        end
+
+        assert { File.exists?(@dist_file) }
+        assert { File.size(@dist_file) > 0 }
+
+        actual_files = list_tar_file(@dist_file)
+        expected_files = %w(
+          jenkins-backup/hudson.model.UpdateCenter.xml
+          jenkins-backup/jobs/
+          jenkins-backup/jobs/example_job/
+          jenkins-backup/jobs/example_job/config.xml
+          jenkins-backup/plugins/
+          jenkins-backup/plugins/dummy.hpi
+          jenkins-backup/plugins/dummy.jpi
+          jenkins-backup/secrets/
+          jenkins-backup/secrets/master.key
+          jenkins-backup/users/
+          jenkins-backup/users/sue445/
+          jenkins-backup/users/sue445/config.xml
+        )
+        expected_files.each do |file|
+          assert("archive file should include #{file}") do
+            actual_files.include?(file)
+          end
+        end
+      end
     end
 
-    test "should create archive file" do
-      dist_file = "#{@temp_dir}/archive.tar.gz"
+    sub_test_case "When exists pinned files" do
+      test "should create archive file" do
+        use_relative_dist_file
+        use_jenkins_home_with_pinned
 
-      assert "jenkins-backup.sh should be success" do
-        system("#{SCRIPT_FILE} #{@jenkins_home} #{dist_file}") == true
-      end
+        assert "jenkins-backup.sh should be success" do
+          system("#{SCRIPT_FILE} #{@jenkins_home} #{@dist_file}") == true
+        end
 
-      assert { File.exists?(dist_file) }
-      assert { File.size(dist_file) > 0 }
+        assert { File.exists?(@dist_file) }
+        assert { File.size(@dist_file) > 0 }
 
-      actual_files = list_tar_file(dist_file)
-      expected_files = %w(
-        jenkins-backup/hudson.model.UpdateCenter.xml
-        jenkins-backup/jobs/
-        jenkins-backup/jobs/example_job/
-        jenkins-backup/jobs/example_job/config.xml
-        jenkins-backup/plugins/
-        jenkins-backup/plugins/dummy.hpi
-        jenkins-backup/plugins/dummy.hpi.pinned
-        jenkins-backup/plugins/dummy.jpi
-        jenkins-backup/plugins/dummy.jpi.pinned
-        jenkins-backup/secrets/
-        jenkins-backup/secrets/master.key
-        jenkins-backup/users/
-        jenkins-backup/users/sue445/
-        jenkins-backup/users/sue445/config.xml
-      )
-      expected_files.each do |file|
-        assert("archive file should include #{file}") do
-          actual_files.include?(file)
+        actual_files = list_tar_file(@dist_file)
+        expected_files = %w(
+          jenkins-backup/hudson.model.UpdateCenter.xml
+          jenkins-backup/jobs/
+          jenkins-backup/jobs/example_job/
+          jenkins-backup/jobs/example_job/config.xml
+          jenkins-backup/plugins/
+          jenkins-backup/plugins/dummy.hpi
+          jenkins-backup/plugins/dummy.hpi.pinned
+          jenkins-backup/plugins/dummy.jpi
+          jenkins-backup/plugins/dummy.jpi.pinned
+          jenkins-backup/secrets/
+          jenkins-backup/secrets/master.key
+          jenkins-backup/users/
+          jenkins-backup/users/sue445/
+          jenkins-backup/users/sue445/config.xml
+        )
+        expected_files.each do |file|
+          assert("archive file should include #{file}") do
+            actual_files.include?(file)
+          end
         end
       end
     end
