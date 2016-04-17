8815e321f206418d77201890b7ce4e859939ee2d
sue445 <sue445@sue445.net>
2016-04-18 01:02:15 +0900

    Test with space contained jobname

--- spec/jenkins-backup_spec.rb
+++ spec/jenkins-backup_spec.rb
@@ -1,26 +1,30 @@
-ARCHIVED_FILES = %w(
-  jenkins-backup/hudson.model.UpdateCenter.xml
-  jenkins-backup/jobs/
-  jenkins-backup/jobs/example_job/
-  jenkins-backup/jobs/example_job/config.xml
-  jenkins-backup/jobs/example_folder/
-  jenkins-backup/jobs/example_folder/config.xml
-  jenkins-backup/jobs/example_folder/jobs/example_job_in_folder/
-  jenkins-backup/jobs/example_folder/jobs/example_job_in_folder/config.xml
-  jenkins-backup/nodes/
-  jenkins-backup/nodes/slave/
-  jenkins-backup/nodes/slave/config.xml
-  jenkins-backup/plugins/
-  jenkins-backup/plugins/dummy.hpi
-  jenkins-backup/plugins/dummy.hpi.pinned
-  jenkins-backup/plugins/dummy.jpi
-  jenkins-backup/plugins/dummy.jpi.pinned
-  jenkins-backup/secrets/
-  jenkins-backup/secrets/master.key
-  jenkins-backup/users/
-  jenkins-backup/users/sue445/
-  jenkins-backup/users/sue445/config.xml
-)
+ARCHIVED_FILES = [
+  "jenkins-backup/hudson.model.UpdateCenter.xml",
+  "jenkins-backup/jobs/",
+  "jenkins-backup/jobs/example_job/",
+  "jenkins-backup/jobs/example_job/config.xml",
+  "jenkins-backup/jobs/space contain job/",
+  "jenkins-backup/jobs/space contain job/config.xml",
+  "jenkins-backup/jobs/example_folder/",
+  "jenkins-backup/jobs/example_folder/config.xml",
+  "jenkins-backup/jobs/example_folder/jobs/example_job_in_folder/",
+  "jenkins-backup/jobs/example_folder/jobs/example_job_in_folder/config.xml",
+  "jenkins-backup/jobs/example_folder/jobs/space contain job in folder/",
+  "jenkins-backup/jobs/example_folder/jobs/space contain job in folder/config.xml",
+  "jenkins-backup/nodes/",
+  "jenkins-backup/nodes/slave/",
+  "jenkins-backup/nodes/slave/config.xml",
+  "jenkins-backup/plugins/",
+  "jenkins-backup/plugins/dummy.hpi",
+  "jenkins-backup/plugins/dummy.hpi.pinned",
+  "jenkins-backup/plugins/dummy.jpi",
+  "jenkins-backup/plugins/dummy.jpi.pinned",
+  "jenkins-backup/secrets/",
+  "jenkins-backup/secrets/master.key",
+  "jenkins-backup/users/",
+  "jenkins-backup/users/sue445/",
+  "jenkins-backup/users/sue445/config.xml",
+]
 
 describe tar_file("/tmp/relative_archive.tar.gz") do
   ARCHIVED_FILES.each do |file|
