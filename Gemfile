a554772900feb1bbf607195ae8bb7165164ee151
sue445 <sue445@sue445.net>
2018-04-19 23:09:33 +0900

    itamae and sercerspec with docker

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
+gem "docker-api"
 gem "itamae"
 gem "rake"
 gem "serverspec"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,34 +1,10 @@
 require 'serverspec'
-require 'net/ssh'
-require 'tempfile'
+require 'docker'
 
-set :backend, :ssh
+set :backend, :docker
 
-if ENV['ASK_SUDO_PASSWORD']
-  begin
-    require 'highline/import'
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV['SUDO_PASSWORD']
-end
-
-host = ENV['TARGET_HOST']
-
-`vagrant up #{host}`
-
-config = Tempfile.new('', Dir.tmpdir)
-config.write(`vagrant ssh-config #{host}`)
-config.close
-
-options = Net::SSH::Config.for(host, [config.path])
-
-options[:user] ||= Etc.getlogin
-
-set :host,        options[:host_name] || host
-set :ssh_options, options
+set :docker_image, ENV['DOCKER_IMAGE']
+set :docker_container, ENV['DOCKER_CONTAINER']
 
 # Disable sudo
 # set :disable_sudo, true
